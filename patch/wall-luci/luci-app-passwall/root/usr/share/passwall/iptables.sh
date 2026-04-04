98d6c891f14187942b61fb33885a4dcacc3f13a7
xiangfeidexiaohuo <jsjson@163.com>
2026-04-04 13:58:44 +0800

    Auto Sync: 2026-04-04 13:58

--- patch/wall-luci/luci-app-passwall/root/usr/share/passwall/iptables.sh
+++ patch/wall-luci/luci-app-passwall/root/usr/share/passwall/iptables.sh
@@ -1047,7 +1047,7 @@ add_firewall_rule() {
 	$ipt_m -A PSW_OUTPUT -m conntrack --ctdir REPLY -j RETURN
 	$ipt_m -A PSW_OUTPUT -m mark --mark 255 -j RETURN
 
-	ip rule add fwmark ${FWMARK} lookup 999 priority 999
+	ip rule add fwmark ${FWMARK} table 999 priority 999
 	ip route add local 0.0.0.0/0 dev lo table 999
 
 	[ "$accept_icmpv6" = "1" ] && {
@@ -1349,10 +1349,10 @@ del_firewall_rule() {
 		done
 	done
 
-	ip rule del fwmark ${FWMARK} lookup 999 priority 999 2>/dev/null
+	ip rule del fwmark ${FWMARK} 2>/dev/null
 	ip route del local 0.0.0.0/0 dev lo table 999 2>/dev/null
 
-	ip -6 rule del fwmark ${FWMARK} lookup 999 priority 999 2>/dev/null
+	ip -6 rule del fwmark ${FWMARK} 2>/dev/null
 	ip -6 route del local ::/0 dev lo table 999 2>/dev/null
 
 	destroy_ipset $IPSET_LOCAL
