5262738b73b69b49c0ffe3b15d284236a32bf6aa
xiangfeidexiaohuo <jsjson@163.com>
2026-08-16 22:25:15 +0800

    Auto Sync: 2026-08-16 22:25

--- patch/wall-luci/luci-app-passwall/luasrc/controller/passwall.lua
+++ patch/wall-luci/luci-app-passwall/luasrc/controller/passwall.lua
@@ -122,6 +122,7 @@ function index()
 	entry({"admin", "services", appname, "geo_view"}, call("geo_view")).leaf = true
 
 	entry({"admin", "services", appname, "fetch_certsha256"}, call("fetch_certsha256")).leaf = true
+	entry({"admin", "services", appname, "gen_wireguard_key"}, call("gen_wireguard_key")).leaf = true
 end
 
 local function http_write_json(content)
@@ -1189,5 +1190,14 @@ function delete_select_shunt_rules()
 		http.redirect(api.url("rule"))
 	else
 		uci_save(true, true)
+	end
+end
+
+function gen_wireguard_key()
+	local key = api.gen_wireguard_key()
+	if key then
+		http_write_json_ok(key)
+	else
+		http_write_json_error()
 	end
 end
